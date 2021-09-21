c63c6581b6ee04d995f3a447910b9fcfcc5334e1
Lena-Sasha <90599754+Lena-Sasha@users.noreply.github.com>
2021-09-21 13:21:10 +0300

    Update Source.cpp

--- Open CV/Source.cpp
+++ Open CV/Source.cpp
@@ -5,6 +5,17 @@ using namespace cv;
 using namespace std;
 int main()
 {
-	int height
+	int height = 520;
+	int width = 840;
+	Mat img(height, width, CV_8UC3);
+	Point TextOrg(100, img.rows / 2);
+	int fontFace =  FONT_HERSHEY_SCRIPT_SIMPLEX;
+	double fontScale = 2;
+	Scalar color(200, 100, 50);
+	putText("Hello World", 0);
+	imshow("Hello World, img");
+	waitKey(0);
+	return 0;
+
 
 }
